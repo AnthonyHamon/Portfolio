1d3c2376205010f5fb665ab19430cccaa39e7fc2
Anthony Hamon <anthony.hamon91@gmail.com>
2025-01-16 14:26:22 +0100

    fix: video not being autoplayed in browser

--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -1,6 +1,6 @@
 import { animate, state, style, trigger, transition } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { PortfolioComponent } from '../portfolio.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
@@ -9,7 +9,7 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   standalone: true,
   imports: [CommonModule, PortfolioComponent, TranslateModule],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.scss', 
+  styleUrl: './project.component.scss',
   animations: [
     trigger('slideInRight', [
       state('hidden', style({
@@ -43,7 +43,7 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 export class ProjectComponent {
 
-  @Input() id='';
+  @Input() id = '';
   @Input() firstProject = false;
   @Input() even = false;
   @Input() odd = false;
@@ -57,25 +57,42 @@ export class ProjectComponent {
     projectDescription: '',
   };
 
+  @ViewChild('video', { static: false }) videoRef!: ElementRef<HTMLVideoElement>;
+
+
 
   isVisible = false;
   private observer!: IntersectionObserver;
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) { }
+
+
+  ngAfterViewInit() {
+    this.startVideo();
+  }
 
   ngOnInit() {
     const projectElement = this.el.nativeElement.querySelector('#project');
-      this.observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            this.isVisible = true;
-          } else {
-            this.isVisible = false;
-          }
-        });
-      }, { threshold: 0.1 });
-      this.observer.observe(projectElement);
-}
+    this.observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.isVisible = true;
+        } else {
+          this.isVisible = false;
+        }
+      });
+    }, { threshold: 0.1 });
+    this.observer.observe(projectElement);
+  }
+
+  startVideo() {
+    const video = this.videoRef.nativeElement;
+    video.muted = true;
+    video.play().catch((error) => {
+      console.error('Autoplay failed:', error);
+    });
+
+  }
 
   ngOnDestroy() {
     if (this.observer) {
